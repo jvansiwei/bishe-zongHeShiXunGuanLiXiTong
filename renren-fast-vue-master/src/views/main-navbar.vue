b0a35e468974b1227838febe8633d5daff86d2cf
tiandaohuashen <1948502009@qq.com>
2019-12-24 18:08:40 +0800

    no message

--- renren-fast-vue-master/src/views/main-navbar.vue
+++ renren-fast-vue-master/src/views/main-navbar.vue
@@ -37,9 +37,10 @@
         </el-submenu> -->
         <el-menu-item class="site-navbar__avatar" index="3">
           <el-dropdown :show-timeout="0" placement="bottom">
-            <!-- <span class="el-dropdown-link">
-              <img src="~@/assets/img/avatar.png" :alt="userName">{{ userName }}
-            </span> -->
+            <span class="el-dropdown-link">
+              <!-- <img src="~@/assets/img/avatar.png" :alt="userName"> -->
+              {{ userName }}
+            </span>
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item @click.native="updateInfoHandle()">修改信息</el-dropdown-item>
               <el-dropdown-item @click.native="updatePasswordHandle()">修改密码</el-dropdown-item>
